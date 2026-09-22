54a471efdcc492eada4c4e3da00d80442c89efb8
Yurii <shchegolev964@gmail.com>
2026-09-23 02:21:04 +0400

    day 25 ci + swagger ui finished

--- src/main/java/com/example/config/OpenApiConfig.java
+++ src/main/java/com/example/config/OpenApiConfig.java
@@ -1,5 +1,10 @@
 package com.example.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
